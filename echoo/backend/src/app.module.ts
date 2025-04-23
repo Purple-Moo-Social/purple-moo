badab30771ae4f87e97dd3cfa4b7b97cc2ec1a3d
Gh057N7h3C0d3 <204660639+Gh057N7h3C0d3@users.noreply.github.com>
2025-04-23 00:02:31 -0500

    feat(db): Configure TypeORM for PostgreSQL

--- echoo/backend/src/app.module.ts
+++ echoo/backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'echoo_admin',
+      password: 'Purple!moo@social24!',
+      database: 'echoo_auth',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
